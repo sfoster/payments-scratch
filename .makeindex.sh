4bfb3191cedff0fbb3c862531ec493e6ad0dec0d
Sam Foster <sfoster@mozilla.com>
2018-09-07 15:34:47 -0700

    A generated markdown directory index

---
+++ .makeindex.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# assemble a markdown directory index
+echo "# PaymentRequest test/demo/example pages and resources"
+echo ""
+
+for filepath in `find -maxdepth 1 -mindepth 1| grep -v '/\.' | sort`; do
+  path=`basename "$filepath"`
+  if [ -f $filepath ]
+  then
+  echo "*  [$path]($path)"
+  # could output directories but as I don't plan to create indices all the way down lets not
+  #else
+  #echo "*  &#128193; [$path]($path)"
+  fi
+done
---
+++ index.md
@@ -0,0 +1,10 @@
+# PaymentRequest test/demo/example pages and resources
+
+*  [cardTypes.html](cardTypes.html)
+*  [completeTimeout.html](completeTimeout.html)
+*  [index.html](index.html)
+*  [index.md](index.md)
+*  [merchantErrors.html](merchantErrors.html)
+*  [pans.html](pans.html)
+*  [sampleNumbers.json](sampleNumbers.json)
+*  [supportedNetworks.html](supportedNetworks.html)
